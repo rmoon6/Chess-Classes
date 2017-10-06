e4c2daf2996588af63bf2ce6d22ac13ad898ab93
Chase <Chase@fixdapp.com>
2017-10-06 10:14:11 -0400

    First Commit

---
+++ src/ChessTester.java
@@ -0,0 +1,91 @@
+public class ChessTester {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testing each piece from the corners of the board (bottom left, top left, bottom right, top right)");
+        System.out.println("Pawn (WHITE):");
+        Pawn p = new Pawn(Color.WHITE);
+        printSquareArray(p.movesFrom(new Square('a', '1')));
+        printSquareArray(p.movesFrom(new Square('a', '8')));
+        printSquareArray(p.movesFrom(new Square('h', '1')));
+        printSquareArray(p.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + p.algebraicName());
+        System.out.println("FEN name: " + p.fenName());
+        System.out.println();
+
+        System.out.println("Pawn (BLACK):");
+        Pawn p2 = new Pawn(Color.BLACK);
+        printSquareArray(p2.movesFrom(new Square('a', '1')));
+        printSquareArray(p2.movesFrom(new Square('a', '8')));
+        printSquareArray(p2.movesFrom(new Square('h', '1')));
+        printSquareArray(p2.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + p2.algebraicName());
+        System.out.println("FEN name: " + p2.fenName());
+        System.out.println();
+
+        System.out.println("Rook:");
+        Rook r = new Rook(Color.WHITE);
+        printSquareArray(r.movesFrom(new Square('a', '1')));
+        printSquareArray(r.movesFrom(new Square('a', '8')));
+        printSquareArray(r.movesFrom(new Square('h', '1')));
+        printSquareArray(r.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + r.algebraicName());
+        System.out.println("FEN name: " + r.fenName());
+        System.out.println();
+
+        System.out.println("Knight:");
+        Knight n = new Knight(Color.WHITE);
+        printSquareArray(n.movesFrom(new Square('a', '1')));
+        printSquareArray(n.movesFrom(new Square('a', '8')));
+        printSquareArray(n.movesFrom(new Square('h', '1')));
+        printSquareArray(n.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + n.algebraicName());
+        System.out.println("FEN name: " + n.fenName());
+        System.out.println();
+
+        System.out.println("Bishop:");
+        Bishop b = new Bishop(Color.WHITE);
+        printSquareArray(b.movesFrom(new Square('a', '1')));
+        printSquareArray(b.movesFrom(new Square('a', '8')));
+        printSquareArray(b.movesFrom(new Square('h', '1')));
+        printSquareArray(b.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + b.algebraicName());
+        System.out.println("FEN name: " + b.fenName());
+        System.out.println();
+
+        System.out.println("Queen:");
+        Queen q = new Queen(Color.WHITE);
+        printSquareArray(q.movesFrom(new Square('a', '1')));
+        printSquareArray(q.movesFrom(new Square('a', '8')));
+        printSquareArray(q.movesFrom(new Square('h', '1')));
+        printSquareArray(q.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + q.algebraicName());
+        System.out.println("FEN name: " + q.fenName());
+        System.out.println();
+
+        System.out.println("King:");
+        King k = new King(Color.WHITE);
+        printSquareArray(k.movesFrom(new Square('a', '1')));
+        printSquareArray(k.movesFrom(new Square('a', '8')));
+        printSquareArray(k.movesFrom(new Square('h', '1')));
+        printSquareArray(k.movesFrom(new Square('h', '8')));
+        System.out.println("Algebraic name: " + k.algebraicName());
+        System.out.println("FEN name: " + k.fenName());
+        System.out.println();
+
+    }
+
+    public static void printSquareArray(Square[] squares) {
+
+        if (squares.length == 0) {
+            System.out.print("None");
+        }
+
+        for (Square s : squares) {
+            System.out.print(s + "\t");
+        }
+        System.out.println();
+
+    }
+
+}
